7c4cdfe3afc47a66eb21fe0d19dfca49989027ad
Vasanth2061 <93462442+Vasanth2061@users.noreply.github.com>
2025-02-13 16:43:44 +0530

    Update ListItems-LastUpdated.ps1

--- Scripts/ListItems-LastUpdated.ps1
+++ Scripts/ListItems-LastUpdated.ps1
@@ -1,12 +1,17 @@
 param (
-    [string]$directoryPath,
-    [string]$outputFilePath
+    [string]$directoryPath = "C:\default\directory\path",
+    [string]$outputFilePath = "C:\default\output\path\output.txt"
 )
 
 # Ensure the directory for the output file exists
 $outputDirectory = Split-Path -Path $outputFilePath -Parent
 if (-not (Test-Path -Path $outputDirectory)) {
     New-Item -Path $outputDirectory -ItemType Directory -Force
+}
+
+# If the output file exists, clear its content
+if (Test-Path -Path $outputFilePath) {
+    Clear-Content -Path $outputFilePath
 }
 
 # Get the list of items and their last modified date, then save to the output file
