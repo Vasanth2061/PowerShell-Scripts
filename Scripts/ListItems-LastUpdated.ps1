2944170b3598b4e579d4e53bbe8f4ed2dc383c48
Vasanth2061 <93462442+Vasanth2061@users.noreply.github.com>
2025-02-13 16:32:36 +0530

    Update ListItems-LastUpdated.ps1

--- Scripts/ListItems-LastUpdated.ps1
+++ Scripts/ListItems-LastUpdated.ps1
@@ -3,5 +3,11 @@ param (
     [string]$outputFilePath
 )
 
+# Ensure the directory for the output file exists
+$outputDirectory = Split-Path -Path $outputFilePath -Parent
+if (-not (Test-Path -Path $outputDirectory)) {
+    New-Item -Path $outputDirectory -ItemType Directory -Force
+}
+
 # Get the list of items and their last modified date, then save to the output file
 Get-ChildItem -Path $directoryPath | Select-Object Name, LastWriteTime | Out-File -FilePath $outputFilePath
